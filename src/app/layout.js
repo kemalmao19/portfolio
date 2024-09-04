780b30b21b3671bbd65679f49eb4953f81d2d2ff
kemalmao19 <sebelumtidur14@gmail.com>
2024-09-04 17:13:13 +0700

    chore: add meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,20 @@
-import { Inter } from 'next/font/google'
-import '../styles/globals.css'
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Kemalmao - Full Stack Developer',
-  description: 'Hello iam kemal a full stack developer from indonesia.',
-}
+  title: "Kemalmao - Full Stack Web Developer",
+  description:
+    "Kemalmao is a skilled full stack web developer from Indonesia with a passion for creating innovative and user-friendly web applications. He has expertise in a wide range of technologies, including JavaScript, React, Node.js, and more. Kemalmao is dedicated to delivering high-quality work and staying up-to-date with the latest industry trends.",
+  keywords:
+    "Kemalmao, full stack developer, web developer, Indonesia, JavaScript, React, Node.js, web development, software development, programming",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
